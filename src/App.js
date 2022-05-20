dfb3cf72eb4355663e7bcaae3e4e5df274967c20
Federico Rotondo <fdrcrotondomza@gmail.com>
2022-05-19 21:57:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+
+function App({value}) {
+
+  const [ counter, setCounter ] = useState( value );
+
+  const add = () => {
+
+      setCounter ( counter + 1 );        
+  }
+
+  const subtract = () => {
+
+      setCounter ( counter - 1 );        
+  }
+
+  const reset = () => {
+
+      setCounter ( value );        
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <h4 className='Legend'>Your best investment</h4>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           How much are you willing to invest?
         </p>
-        <a
+        <div>          
+          <button onClick= { subtract } >-1</button>
+          <button onClick= { reset } > RESET </button>
+          <button onClick= { add } > +1 </button>
+          <h2> { counter }  millions of dollars</h2>  
+        </div>
+         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org" //reemplazar por enlace al repositorio
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out this project on GitHub
         </a>
       </header>
     </div>
